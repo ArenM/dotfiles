3a64f1f30484fd6a65e14b1bbe445ee47f13e372
Aren Moynihan <tele-kid@peacevolution.org>
2019-04-17 15:03:12 -0400

    Fine Tune zsh

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -5,9 +5,11 @@ source <(antibody init)
 antibody bundle < ~/.zsh_plugins
 
 # Themes
-autoload -Uz up-line-or-beginning-search down-line-or-beginning-search
+autoload -Uz up-line-or-beginning-search down-line-or-beginning-search compinit
 zle -N up-line-or-beginning-search
 zle -N down-line-or-beginning-search
+
+compinit -D
 
 [[ -n "${key[Up]}"   ]] && bindkey "${key[Up]}"   up-line-or-beginning-search
 [[ -n "${key[Down]}" ]] && bindkey "${key[Down]}" down-line-or-beginning-search
