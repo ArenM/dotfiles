d0b83a0417f9c47bd216476982c067e2823f0c2a
= <=>
2019-09-11 21:04:22 -0400

    Continue working on getting opengl version

--- _grains/gles3.py
+++ _grains/gles3.py
@@ -4,10 +4,16 @@ import subprocess
 import re
 
 def enviroment():
-    glinfo = subprocess.check_output('glxinfo')
+    glinfo = subprocess.check_output('glxinfo; exit 0', stderr=subprocess.STDOUT, shell=True)
    
-    maxver = re.findall(r'Max core profile version.*$', glinfo, flags=re.MULTILINE)[0]
-    maxver = float(maxver.split(': ')[1])
+    try:
+        maxver = re.findall(r'Max core profile version.*$', glinfo, flags=re.MULTILINE)[0]
+        maxver = float(maxver.split(': ')[1])
+    except:
+        import os
+        with open('glinfo.txt','w') as f:
+            f.write(glinfo)
+        return {'gles3': os.getcwd()}
 
     versupp = maxver > 3
     
