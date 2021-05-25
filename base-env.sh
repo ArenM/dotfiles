c0dfa4e5972fa871bec6683d2a4f66563af0683a
ArenM <aren@peacevolution.org>
2021-05-25 19:16:34 -0400

    [nvim] make vimwiki opt-in

--- base-env.sh
+++ base-env.sh
@@ -1,12 +1,15 @@
 #! /usr/bin/env sh
 
-# Static Varibles
+# Static Variables
 GLES3=true
 GTK_THEME=Breeze
 THEME=atelier-dune-light
 RICE=false
 
-# Derived Varibles
+# Application Specific Customization
+VIMWIKI=false
+
+# Derived Variables
 if [ "$GLES3" = "true" ]; then
   TERMINAL="alacritty"
 else
